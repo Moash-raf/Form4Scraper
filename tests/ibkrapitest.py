7f74562cde4a4027afca55562495910fb6d0d231
Moash-raf <mai307@nyu.edu>
2025-10-23 13:08:34 -0400

    Created run loop and basic trading requests

--- tests/ibkrapitest.py
+++ tests/ibkrapitest.py
@@ -16,4 +16,38 @@ class IBapi(EClient, EWrapper):
     
     def accountSummary(self, reqId, account, tag, value, currency):
         print(f"Account Summary. {tag}: {value} {currency}")
-        
+
+    def run_loop(app):
+        app.run()
+
+app = IBapi()
+app.connect("127.0.0.1", 7497, clientId = 1)
+
+api_thread = threading.Thread(target=run_loop, args=(app,), daemon=True)
+api_thread.start()
+
+time.sleep(2)
+
+app.reqAccountSummary(1, "ALL", "$LEDGER")
+
+# Defining contract
+
+contract = Contract()
+contract.symbol = "AAPL"
+contract.secType = "STK"
+contract.exchange = "SMARK"
+contract.currency = "USD"
+
+# Defining order
+
+order = Order()
+order.action = "BUY"
+order.orderType = "MKT"
+order.totalQuantity = 1
+
+# Placing order
+
+app.placeOrder(app.next_order_id, contract, order)
+app.next_order_id += 1
+
+app.disconnect()
